fd8b8806f07526e094041af3e3a79c600d989b5c
Manish Kumar Sah <52593756+manishkumarsahh@users.noreply.github.com>
2021-12-13 19:25:12 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
  
 
-//establishing connection with mongoose
+//establishing connection with mongoose 
 const mongoose = require('mongoose');
 const db = process.env.MONGODB_URI || 'mongodb+srv://manish:fleo123@cluster0.ebci7.mongodb.net/assignmentnode?retryWrites=true&w=majority';
 mongoose.connect(db, {
